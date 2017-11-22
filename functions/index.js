4e675b18f973b47abec3685fdb2200c5d310fb9c
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-22 17:28:33 +0100

    Refactoring, delete / comment some unused code

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,74 @@
 "use strict";
 
-const functions = require("firebase-functions"); // Cloud Functions for Firebase library
-const DialogflowApp = require("actions-on-google").DialogflowApp; // Google Assistant helper library
+const functions = require("firebase-functions");
+const DialogflowApp = require("actions-on-google").DialogflowApp;
+
+const attendees = [
+  "TODO"
+];
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
   (request, response) => {
     console.log(
       "Dialogflow Request headers: " + JSON.stringify(request.headers)
     );
     console.log("Dialogflow Request body: " + JSON.stringify(request.body));
-    if (request.body.result) {
-      console.error("Wrong API Version (V1): processV1Request does not exist");
-    } else if (request.body.queryResult) {
+    if (request.body.queryResult) {
       processV2Request(request, response);
     } else {
       console.log("Invalid Request");
       return response
         .status(400)
-        .end("Invalid Webhook Request (expecting v1 or v2 webhook request)");
+        .end("Invalid Webhook Request (expecting v2 webhook request)");
     }
   }
 );
 
-/*
-* Function to handle v2 webhook requests from Dialogflow
-*/
 function processV2Request(request, response) {
+  function sendResponse(responseToUser) {
+    const responseJson = {};
+    responseJson.fulfillmentText = responseToUser.fulfillmentText;
+    if (responseToUser.fulfillmentMessages) {
+      responseJson.fulfillmentMessages = responseToUser.fulfillmentMessages;
+    }
+    if (responseToUser.outputContexts) {
+      responseJson.outputContexts = responseToUser.outputContexts;
+    }
+    console.log(`Response to Dialogflow: ${JSON.stringify(responseJson)}`);
+    response.json(responseJson);
+  }
+
   // An action is a string used to identify what needs to be done in fulfillment
-  let action = request.body.queryResult.action
+  const action = request.body.queryResult.action
     ? request.body.queryResult.action
     : "default";
   // Parameters are any entites that Dialogflow has extracted from the request.
-  let parameters = request.body.queryResult.parameters || {}; // https://dialogflow.com/docs/actions-and-parameters
+  // const parameters = request.body.queryResult.parameters || {}; // https://dialogflow.com/docs/actions-and-parameters
   // Contexts are objects used to track and store conversation state
-  let inputContexts = request.body.queryResult.contexts; // https://dialogflow.com/docs/contexts
+  // const inputContexts = request.body.queryResult.contexts; // https://dialogflow.com/docs/contexts
   // Get the request source (Google Assistant, Slack, API, etc)
-  let requestSource = request.body.originalDetectIntentRequest
-    ? request.body.originalDetectIntentRequest.source
-    : undefined;
+  // const requestSource = request.body.originalDetectIntentRequest
+  //   ? request.body.originalDetectIntentRequest.source
+  //   : undefined;
   // Get the session ID to differentiate calls from different users
-  let session = request.body.session ? request.body.session : undefined;
+  const session = request.body.session ? request.body.session : undefined;
   // Create handlers for Dialogflow actions as well as a 'default' handler
   const actionHandlers = {
     // The default welcome intent has been matched, welcome the user (https://dialogflow.com/docs/events#default_welcome_intent)
-    "input.welcome": () => {
-      sendResponse("Hello, Welcome to my Dialogflow agent!"); // Send simple response to user
-    },
+    // "input.welcome": () => {
+    //   sendResponse({ fulfillmentText: "Hello, Welcome to my Dialogflow agent!" }); // Send simple response to user
+    // },
     // The default fallback intent has been matched, try to recover (https://dialogflow.com/docs/intents#fallback_intents)
-    "input.unknown": () => {
-      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
-      sendResponse("I'm having trouble, can you try that again?"); // Send simple response to user
-    },
+    // "input.unknown": () => {
+    //   // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
+    //   sendResponse({ fulfillmentText: "I'm having trouble, can you try that again?" }); // Send simple response to user
+    // },
+    // See also: contexts (https://dialogflow.com/docs/contexts)
     rollTheDice: () => {
-      const attendees = [
-        "TODO"
-      ];
       const winner = attendees[Math.floor(Math.random() * attendees.length)];
+      console.log(`The winner is: ${winner}`);
 
-      const responseToUser = {
+      sendResponse({
         outputContexts: [
           {
             name: `${session}/contexts/winner`,
@@ -64,76 +76,32 @@ function processV2Request(request, response) {
             parameters: { winner: winner }
           }
         ],
-        fulfillmentMessages: richResponsesV2
-        // fulfillmentText: "Sure! Rolling the dice... Done!"
-      };
-      sendResponse(responseToUser);
+        fulfillmentMessages: determineWinnerMessages
+      });
     },
-    // Default handler for unknown or undefined actions
     default: () => {
-      const responseToUser = {
-        //fulfillmentMessages: richResponsesV2, // Optional, uncomment to enable
-        //outputContexts: [{ 'name': `${session}/contexts/weather`, 'lifespanCount': 2, 'parameters': {'city': 'Rome'} }], // Optional, uncomment to enable
-        fulfillmentText:
-          "This is from Dialogflow's Cloud Functions for Firebase editor! :-)" // displayed response
-      };
-      sendResponse(responseToUser);
+      sendResponse({ fulfillmentText: `Action ${action} is not defined` });
     }
   };
-  // If undefined or unknown action use the default handler
-  if (!actionHandlers[action]) {
-    action = "default";
-  }
-  // Run the proper handler function to handle the request from Dialogflow
-  actionHandlers[action]();
-  // Function to send correctly formatted responses to Dialogflow which are then sent to the user
-  function sendResponse(responseToUser) {
-    // if the response is a string send it as a response to the user
-    if (typeof responseToUser === "string") {
-      let responseJson = { fulfillmentText: responseToUser }; // displayed response
-      response.json(responseJson); // Send response to Dialogflow
-    } else {
-      // If the response to the user includes rich responses or contexts send them to Dialogflow
-      let responseJson = {};
-      // Define the text response
-      responseJson.fulfillmentText = responseToUser.fulfillmentText;
-      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
-      if (responseToUser.fulfillmentMessages) {
-        responseJson.fulfillmentMessages = responseToUser.fulfillmentMessages;
-      }
-      // Optional: add contexts (https://dialogflow.com/docs/contexts)
-      if (responseToUser.outputContexts) {
-        responseJson.outputContexts = responseToUser.outputContexts;
-      }
-      // Send the response to Dialogflow
-      console.log("Response to Dialogflow: " + JSON.stringify(responseJson));
-      response.json(responseJson);
-    }
-  }
+
+  const actionHandler = actionHandlers[action] || actionHandlers['default']
+  actionHandler();
 }
-const richResponsesV2 = [
+
+const determineWinnerResponse = {
+  text_to_speech:
+    '<speak>Sure! Rolling the dice... <audio clipBegin="10s" clipEnd="16s" src="https://actions.google.com/sounds/v1/cartoon/drum_roll.ogg">drumroll</audio></speak>',
+  display_text: "Sure! Rolling the dice... Done."
+};
+
+// See rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+const determineWinnerMessages = [
   {
     platform: "PLATFORM_UNSPECIFIED",
-    simple_responses: {
-      simple_responses: [
-        {
-          text_to_speech:
-            '<speak>Sure! Rolling the dice... <audio clipBegin="10s" clipEnd="16s" src="https://actions.google.com/sounds/v1/cartoon/drum_roll.ogg">drumroll</audio></speak>',
-          display_text: "Sure! Rolling the dice... Done."
-        }
-      ]
-    }
+    simple_responses: { simple_responses: [determineWinnerResponse] }
   },
   {
     platform: "ACTIONS_ON_GOOGLE",
-    simple_responses: {
-      simple_responses: [
-        {
-          text_to_speech:
-            '<speak>Sure! Rolling the dice... <audio clipBegin="10s" clipEnd="16s" src="https://actions.google.com/sounds/v1/cartoon/drum_roll.ogg">drumroll</audio></speak>',
-          display_text: "Sure! Rolling the dice... Done."
-        }
-      ]
-    }
+    simple_responses: { simple_responses: [determineWinnerResponse] }
   }
 ];
